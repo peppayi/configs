e544466feed371a1d658c9321092647e9a80f9bc
xmucoder <xmucoder@gmail.com>
2020-07-08 22:47:57 +0800

    improve: install ctags based on system type and make the setup process re-runnable

--- vim/setup.sh
+++ vim/setup.sh
@@ -2,13 +2,30 @@
 
 set -e
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# install vim package manager
+PMDIR=~/.vim/bundle/Vundle.vim
+if [ -d "${PMDIR}" ]; then
+    pushd "${PMDIR}" > /dev/null && git pull --rebase && popd > /dev/null
+else  
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 
+# install ctags
+case `uname -s` in
+"Linux")
+    sudo apt-get install ctags
+    ;;
+"Darwin")
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install ctags
+    ;;
+*)
+    ;;
+esac
+
+# config and install plugins
 mkdir -p ~/.vim/customize
 cp plugin.vim ~/.vim/customize/
-
 cp vimrc.vim ~/.vimrc
-
 vim +PluginInstall +qall
 
 echo "Done!"
